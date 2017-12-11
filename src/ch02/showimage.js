3f7b7a57dc096e9055a9b88822e963a99be7fb52
soulduse <soulduse@naver.com>
2017-12-11 23:06:15 +0900

    이미지 파일 긁어오기

---
+++ src/ch02/showimage.js
@@ -0,0 +1,18 @@
+// load module
+var client = require('cheerio-httpcli');
+var urlType = require('url');
+
+// download
+var url = "https://ko.wikipedia.org/wiki/"+encodeURIComponent("강아지");
+var param = {};
+
+client.fetch(url, param, function(err, $, res){
+    if(err){console.log("error"); return;}
+
+    // 링크를 추출하여 표시
+    $("img").each(function(idx){
+        var src = $(this).attr('src');
+        src = urlType.resolve(url, src);
+        console.log(src);
+    })
+})
